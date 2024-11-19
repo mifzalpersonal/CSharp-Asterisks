ee6194887c077a55506f038ed7e5aa78650d9d7d
mifzalpersonal <mizakigaming89@gmail.com>
2024-11-19 12:38:54 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int tinggi;
+
+
+        Console.Write("Masukkan tinggi piramida: ");
+        tinggi = int.Parse(Console.ReadLine());
+
+
+        for (int i = 1; i <= tinggi; i++)
+        {
+           
+            for (int j = 1; j <= tinggi - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+
+           
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+
+
+           
+            Console.Write("  "); 
+
+
+           
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+
+
+            Console.WriteLine(); 
+        }
+    }
+}
